35f7694aa6ea900b6a48a7038bb83c66e8a88518
KarthikRathinam <karthik2397@gmail.com>
2022-07-15 11:44:14 +0530

    Initial Commit: Stock service

---
+++ src/main/java/com/karthik/stock/stockservice/StockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.karthik.stock.stockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockServiceApplication.class, args);
+	}
+
+}
